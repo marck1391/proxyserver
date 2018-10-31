66930ef4960cf38d65b3c2a3a3d05664bba09f1d
Marco Orta <marck1391@gmail.com>
2018-10-31 16:51:04 -0600

    Init

---
+++ server.js
@@ -0,0 +1,28 @@
+const https = require('https')
+const httpProxy = require('http-proxy')
+const fs = require('fs')
+const config = require('./config.json')
+
+var proxy = new httpProxy.createProxyServer({
+  target: config.target
+})
+
+var options = {/*key, cert, ca*/}
+
+Object.keys(config.ssl).forEach(function(keyName){
+  var key = config.ssl[keyName]
+  if(fs.existsSync(onfig.ssl[keyName])){
+    key = fs.readFileSync(config.ssl[keyName])
+  }
+  options[keyName] = key
+})
+
+var server = https.createServer(options, function(req, res){
+  proxy.web(req, res)
+})
+
+server.on('upgrade', function(req, socket, head){
+  proxy.ws(req, socket, head);
+})
+
+server.listen(config.port||443)
